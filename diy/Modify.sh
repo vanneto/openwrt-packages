8b5827635b6ba0a92812c859744ccfbb7657d878
wx611975 <wgj611975@gmail.com>
2022-03-08 21:33:16 +0800

    openwrt

---
+++ diy/Modify.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# --------------------------------------------------------
+# Script for creating ACL file for each LuCI APP
+sed -i \
+-e 's?include \.\./\.\./\(lang\|devel\)?include $(TOPDIR)/feeds/packages/\1?' \
+-e "s/\(PKG_HASH\|PKG_MD5SUM\|PKG_MIRROR_HASH\):=.*/\1:=skip/" \
+-e 's?2. Clash For OpenWRT?3. Applications?' \
+-e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' \
+-e 's/ca-certificates/ca-bundle/' \
+*/Makefile
+
+sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
+sed -i 's/luci-lib-ipkg/luci-base/g' luci-app-store/Makefile
+sed -i 's/adguardhome.yaml/AdGuardHome.yaml/g' adguardhome/files/adguardhome.init
+bash diy/create_acl_for_luci.sh -a >/dev/null 2>&1
+bash diy/convert_translation.sh -a >/dev/null 2>&1
+rm -rf create_acl_for_luci.err & rm -rf create_acl_for_luci.ok
+rm -rf create_acl_for_luci.warn
+
+exit 0
